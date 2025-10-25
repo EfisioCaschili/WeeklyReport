7fe12b859941ce5b1a1c81d00bcc33a0fabc3ecc
EfisioCaschili <118173025+EfisioCaschili@users.noreply.github.com>
2025-10-25 23:29:28 +0200

    Update methods to download from Sharepoint

--- dataParser.py
+++ dataParser.py
@@ -9,35 +9,14 @@ import requests
 from urllib.parse import urlparse
 
 class Data():
-    def download_from_sharepoint_old(self,site_url,file_url,new_filename,username,password):
-        from office365.runtime.auth.authentication_context import AuthenticationContext
-        from office365.sharepoint.client_context import ClientContext
-        from office365.sharepoint.files.file import File    
-        import os
-        import tempfile
-        ctx_auth = AuthenticationContext(site_url)
-        if ctx_auth.acquire_token_for_user(username, password):
-            ctx = ClientContext(site_url, ctx_auth)
-            with open(new_filename, 'wb') as output_file:
-                file = (
-                        ctx.web.get_file_by_server_relative_url(file_url).download(output_file).execute_query()
-                )
-            print("[Ok] file has been downloaded into: {0}".format(new_filename))    
+      
     
     def get_access_token(self,clientID, clientSecret, tenantID):
         """
-        Ottiene un token App-Only Azure AD v2 per Microsoft Graph
+        Obtains a token App-Only Azure AD v2 for Microsoft Graph
         """
-        token_url = f"https://login.microsoftonline.com/{tenantID}/oauth2/v2.0/token"
-        data = {
-            "client_id": clientID,
-            "client_secret": clientSecret,
-            "scope": "https://graph.microsoft.com/.default",
-            "grant_type": "client_credentials"
-        }
-        r = requests.post(token_url, data=data)
-        r.raise_for_status()
-        return r.json()["access_token"]
+        pass
+        
 
     def get_site_id(self,site_url, access_token):
         """
@@ -45,47 +24,42 @@ class Data():
         """
         parsed = urlparse(site_url)
         hostname = parsed.netloc
-        path = parsed.path.strip("/")  # es. 'sites/GBTS'
+        path = parsed.path.strip("/") 
         graph_url = f"https://graph.microsoft.com/v1.0/sites/{hostname}:/{path}"
         headers = {"Authorization": f"Bearer {access_token}"}
         r = requests.get(graph_url, headers=headers)
         r.raise_for_status()
         return r.json()["id"]
 
-    def download_from_sharepoint(self, site_url, file_path, new_filename, clientID, clientSecret, tenantID):
+
+    def download_from_sharepoint(self, site_url, file_path, new_filename, clientID, clientSecret, tenantID, drive=""):
         """
         Scarica un file da SharePoint Online via Microsoft Graph API. clientSecret scade il 20/10/2027
         """
         try:
-            # 1️⃣ Ottieni token
+            # 1️⃣ Obtain token
             access_token = self.get_access_token(clientID, clientSecret, tenantID)
             print("Token successfully obtained")
 
-            # 2️⃣ Recupera site_id
+            # 2️⃣ Retrieve site_id
             site_id = self.get_site_id(site_url, access_token)
             #print(f"Site ID obtained: {site_id}")
 
-            # 3️⃣ Recupera tutti i drive del sito
+            
             drives_url = f"https://graph.microsoft.com/v1.0/sites/{site_id}/drives"
             headers = {"Authorization": f"Bearer {access_token}"}
             drives_response = requests.get(drives_url, headers=headers)
             drives_response.raise_for_status()
             drives = drives_response.json()["value"]
 
-            # 4️⃣ Trova il drive "Documents"
-            documents_drive = next((d for d in drives if d["name"] == "Documents"), None)
+            
+            documents_drive = next((d for d in drives if d["name"] == drive), None)
             if not documents_drive:
-                raise Exception("Drive 'Documents' not found in the site.")
+                raise Exception(f"Drive not found in the site.")
 
             drive_id = documents_drive["id"]
-            #print(f"Drive ID trovato: {drive_id}")
-
-            # 5️⃣ URL di download del file
             graph_url = f"https://graph.microsoft.com/v1.0/drives/{drive_id}/root:/{file_path}:/content"
-            #print(f"📥 Download da: {graph_url}")
-
             r = requests.get(graph_url, headers=headers, stream=True)
-            #print(f"➡️ Status code: {r.status_code}")
 
             if r.status_code == 200:
                 with open(new_filename, "wb") as f:
@@ -531,3 +505,4 @@ class ParsingData():
         plt.close()
         img_stream.seek(0)
         return img_stream
+
